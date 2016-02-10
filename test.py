a8c76b1ad9639dd214c825da3c346fdb9550f0f4
Timothy Wegher <timothy.wegher@bz-cs-cslab5.coe.montana.edu>
2016-02-10 16:19:49 -0700

    Done!!!! Need to make a report

---
+++ test.py
@@ -0,0 +1,51 @@
+import  lex
+import filecmp
+import sys
+infile = sys.argv[1]
+
+
+
+# List of token names.   This is always required
+tokens = ('IDENTIFIER',
+		  'INTLITERAL',
+		  'FLOATLITERAL',
+		  'STRINGLITERAL',
+		  'COMMENT',     
+		  'KEYWORD',
+		  'OPERATOR')
+		  
+t_KEYWORD= (r"PROGRAM|BEGIN|END(?!WHILE|IF)|FUNCTION|READ|WRITE|IF|ELSE|ENDIF|WHILE|ENDWHILE|CONTINUE|BREAK|RETURN|INT|VOID|STRING|FLOAT")
+t_IDENTIFIER =(r"[a-zA-Z]+[a-zA-Z0-9]*")
+t_INTLITERAL = (r"[0-9]+")
+t_FLOATLITERAL = (r"[0-9]*\.[0-9]+|\.[0-9]+")
+t_STRINGLITERAL = (r"\"[^\"]*\"")
+t_OPERATOR = (r":=|\+|-(?!-)|\*|/|=|!=|<(?!=)|>(?!=)|\(|\)|;|,|<=|>=")
+t_ignore_COMMENT = (r"--.*\n")
+t_ignore  = (" \t\n")
+
+lexer = lex.lex()
+
+
+
+with open(infile, 'r') as myfile:
+    data = myfile.read()
+file = open('fibonacci.txt' , 'a')
+
+
+lexer.input(data)
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    #file.write('Token Type: ' + tok.type + '\n')
+    #file.write('Value: ' + tok.value + '\n')
+  chm
+file.close()
+
+#print (filecmp.cmp('fibonacci.txt', 'loop2.txt'))
+#file.close()
+
+
+
+
+
