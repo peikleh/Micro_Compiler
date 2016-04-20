0d26cf912fb0e84631445a4867038c9c1d2f0ce8
peikleh <nevinleh@gmail.com>
2016-04-20 14:31:01 -0600

    Started new folder for step 4

---
+++ FINAL/parser.py
@@ -0,0 +1,237 @@
+
+import ply.yacc as yacc
+from lexer import tokens
+import sys
+from symbol_table import Symbol_Table
+
+
+infile = sys.argv[1]
+with open(infile, 'r') as myfile:
+    data = myfile.read()
+accepted = True
+table = Symbol_Table()
+
+#Following functions are the grammar for the LITTLE language
+def p_program(p):
+    'program : PROGRAM id BEGIN pgm_body END'
+   
+def p_id(p):
+    'id : IDENTIFIER'
+    p[0] = p[1]
+    
+def p_pgm_body(p):
+    'pgm_body : decl func_declarations'
+
+def p_decl(p):
+    '''decl : string_decl decl 
+    | var_decl decl 
+    | empty'''
+
+def p_string_decl(p):
+    'string_decl : STRING id EQ_EQ str SEMI'
+    table.add_str(p[2], p[4])
+
+def p_str(p):
+    'str : STRINGLITERAL'
+    p[0] = p[1]
+def p_var_decl(p):
+    'var_decl : var_type id_list SEMI'
+    table.add_var(p[1], p[2])
+
+def p_var_type(p):
+    '''var_type : FLOAT 
+    | INT'''
+    p[0] = p[1]
+
+def p_any_type(p):
+    '''any_type : var_type 
+    | VOID'''
+
+def p_id_list(p):
+    'id_list : id id_tail'
+    p[0] = [p[1]] + p[2]
+
+
+def p_id_tail(p):
+    '''id_tail : COMM id id_tail 
+    | empty'''
+    if len(p) != 2:
+        p[0] = [p[2]] + p[3]
+    else:
+        p[0] = []
+
+def p_param_decl_list(p):
+    '''param_decl_list : param_decl param_decl_tail 
+    | empty'''
+
+def p_param_decl(p):
+    'param_decl : var_type id'
+    table.add_var(p[1], [p[2]])
+
+def p_param_decl_tail(p):
+    '''param_decl_tail : COMM param_decl param_decl_tail 
+    | empty'''
+
+def p_func_declarations(p):
+    '''func_declarations : func_decl func_declarations 
+    | empty'''
+
+def p_func_decl(p):
+    'func_decl : s_func L_PAR param_decl_list R_PAR BEGIN func_body END'
+    table.func_end()
+
+#The following function was added so we could know where functions started
+def p_s_func(p):
+    's_func : FUNCTION any_type id'
+    
+    table.func_start(p[3])
+
+def p_func_body(p):
+    'func_body : decl stmt_list'
+
+def p_stmt_list(p):
+    '''stmt_list : stmt stmt_list 
+    | empty'''
+
+def p_stmt(p):
+    '''stmt : base_stmt
+    | if_stmt
+    | while_stmt'''
+
+def p_base_stmt(p):
+    '''base_stmt : assign_stmt 
+    | read_stmt 
+    | write_stmt 
+    | return_stmt'''
+
+def p_assign_stmt(p):
+    'assign_stmt : assign_expr SEMI'
+
+def p_assign_expr(p):
+    'assign_expr : id EQ_EQ expr'
+
+def p_read_stmt(p):
+    'read_stmt : READ L_PAR id_list R_PAR SEMI'
+
+def p_write_stmt(p):
+    'write_stmt : WRITE L_PAR id_list R_PAR SEMI'
+
+def p_return_stmt(p):
+    'return_stmt : RETURN expr SEMI'
+
+def p_expr(p):
+    'expr : expr_prefix factor'
+
+def p_expr_prefix(p):
+    '''expr_prefix : expr_prefix factor addop 
+    | empty'''
+
+def p_factor(p):
+    'factor : factor_prefix postfix_expr'
+
+def p_factor_prefix(p):
+    '''factor_prefix : factor_prefix postfix_expr mulop 
+    | empty'''
+
+def p_postfix_expr(p):
+    '''postfix_expr : primary 
+    | call_expr'''
+
+def p_call_expr(p):
+    'call_expr : id L_PAR expr_list R_PAR'
+
+def p_expr_list(p):
+    '''expr_list : expr expr_list_tail 
+    | empty'''
+
+def p_expr_list_tail(p):
+    '''expr_list_tail : COMM expr expr_list_tail 
+    | empty'''
+
+def p_primary(p):
+    '''primary : L_PAR expr R_PAR 
+    | id 
+    | INTLITERAL 
+    | FLOATLITERAL'''
+
+def p_addop(p):
+    '''addop : PLUS
+    | MINUS'''
+
+def p_mulop(p):
+    ''' mulop : MULT
+    | DIV'''
+
+def p_if_stmt(p):
+    'if_stmt : s_if L_PAR cond R_PAR decl stmt_list else_part ENDIF'
+    table.block_end()
+
+#The following function was added so we could know where if statements started
+def p_s_if(p):
+    's_if : IF'
+    table.block_start()
+
+def p_else_part(p):
+    '''else_part : s_else decl stmt_list 
+    | empty''' 
+    table.block_end()
+
+#The following function was added so we could know where else statements started
+def p_s_else(p):
+    's_else : ELSE'
+    table.block_start()
+
+def p_cond(p):
+    'cond : expr compop expr'
+
+def p_compop(p):
+    ''' compop : LESS
+    | MORE 
+    | EQ 
+    | N_EQ 
+    | L_EQ 
+    | R_EQ'''
+
+def p_while_stmt(p):
+    'while_stmt : s_while L_PAR cond R_PAR decl stmt_list ENDWHILE'
+    table.block_end()
+def p_s_while(p):
+    's_while : WHILE'
+    table.block_start()
+
+def p_empty(p):
+    'empty :'
+    pass
+
+
+def p_error(p):
+    if p:
+         global accepted 
+         accepted = False
+         parser.errok()
+
+
+    
+parser = yacc.yacc()
+result = ''
+while True:
+
+    try:
+        s = data
+       
+    except EOFError:
+        break
+    if not s: continue
+    
+    parser.parse(s, tracking = True)
+ 
+    """if (accepted == False):
+        print ('Not accepted')
+    else:
+        print ('Accepted')
+        """
+
+
+    break
+
+table.print_output()
